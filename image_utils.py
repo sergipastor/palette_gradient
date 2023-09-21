a28588866064fd3996199cc73446ed1df35f0e11
sergipastor <sergi@nennisiwok.com>
2023-09-21 18:10:19 +0200

    first commit

---
+++ image_utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+from PIL import ImageColor
+import matplotlib.pyplot as plt
+
+ARRWIDTH = 1920
+ARRHEIGHT = 1080
+
+
+def create_channel_image(values, width, height):
+    (upperleft, upperright, lowerleft, lowerright) = values
+
+    array = np.linspace(
+        np.linspace(lowerleft, lowerright, width), 
+        np.linspace(upperleft, upperright, width),
+        height,
+        dtype=int
+    )
+    return array[:, :, None]
+
+
+def create_image(hex_colors: list):
+    rgb_colors = [
+        ImageColor.getcolor(colour, "RGB")
+        for colour in hex_colors
+    ]
+    colors_by_channel = zip(*rgb_colors)
+    channel_images = list(map(lambda x: create_channel_image(x, ARRWIDTH, ARRHEIGHT), colors_by_channel))
+    img = np.concatenate(channel_images, axis=2)
+    return img
+
+
+def store_image(img, filename: str) -> None:
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.axis('off')
+
+    fig.patch.set_alpha(0)
+    ax.patch.set_alpha(0)
+
+    ax.imshow(img, aspect='auto', interpolation='none')
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.savefig(filename, bbox_inches='tight', pad_inches=0, transparent=True)
+    plt.clf()
+    plt.close()
---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+
+from image_utils import create_image, store_image
+
+
+class ValidateColorsLength(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        num_values = len(values)
+        if not (2 <= num_values <= 4):
+            raise argparse.ArgumentTypeError(
+                f"Number of colors must range between 2 and 4: {num_values} given."
+            )
+        setattr(namespace, self.dest, values)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--output_folder",
+        type=str,
+        required=True,
+        help="Path to the output folder."
+    )
+    parser.add_argument(
+        "--colors",
+        nargs="*",
+        action=ValidateColorsLength,
+        default=['#000000', '#000000', '#2FF924', '#DDDBF1'],
+    )
+    parser.add_argument(
+        "--distribution",
+        choices=["top-bottom", "bottom-top", "right-left", "left-right"],
+        default="top-bottom",
+    )
+
+    args = parser.parse_args()
+    img = create_image(args.colors)
+    store_image(img, "uhaha.png")
+
+
+if __name__ == "__main__":
+    main()
