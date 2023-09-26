00cf6870127b9123f2238fd6c120bd6241b2c566
sergipastor <sergi@nennisiwok.com>
2023-09-26 19:39:09 +0200

    add argument help and rename output filename argument.

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ from utils.image import create_image, store_image
 from utils.colors import process_colors, ValidateColors
 from utils.constants import (
     DEFAULT_COLORS, DEFAULT_DISTRIBUTION, DISTRIBUTION_CHOICES
-            )
+)
 
 
 def main():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument(
-        "--output_folder",
+        "--output_filename",
         type=str,
         required=True,
-        help="Path to the output folder."
+        help="Path to the output filename."
     )
     parser.add_argument(
         "--colors",
         nargs="*",
         action=ValidateColors,
         default=DEFAULT_COLORS,
+        help="Hexadecimal values of the colors to be used in the resulting image."
     )
     parser.add_argument(
         "--distribution",
@@ -30,7 +31,7 @@ def main():
     args = parser.parse_args()
     colors = process_colors(args.colors, args.distribution)
     img = create_image(colors)
-    store_image(img, "uhaha.png")
+    store_image(img, args.output_filename)
 
 
 if __name__ == "__main__":
