bece958429fec3d37d0c986da6d2842500f4890d
sergipastor <sergi@nennisiwok.com>
2023-10-03 18:32:16 +0200

    set distribution help as constant

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,10 @@ HEIGHT = 1080
 DEFAULT_COLORS = ['#000000', '#2FF924', '#DDDBF1']
 DEFAULT_DISTRIBUTION = "top-bottom"
 DISTRIBUTION_CHOICES = ["top-bottom", "bottom-top", "right-left", "left-right"]
+DISTRIBUTION_HELP = """
+    Distribution of the colors provided in the resulting image.
+    top-bottom: first colors at the top, last colors at the bottom.
+    bottom-top: first colors at the bottom, last colors at the top.
+    right-left: first colors on the right side, last colors on the left side.
+    left-right: first colors on the left side, last colors on the right side.
+"""
